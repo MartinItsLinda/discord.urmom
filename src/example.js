cd480b77274bac69f194e33e8a54450ee334a504
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-03-11 02:50:54 +0330

    Cleanups
    Cleaned up example.

--- src/example.js
+++ src/example.js
@@ -1,25 +1,27 @@
-const urmom = require(`discord-urmom`);
-const { join } = require("path");
+const urmom = require('discord-urmom');
+const { join } = require('path');
+// Basically your config file
+// const { token } = require('path-to-config-file');
 
-const urmom = new urmom.Client({
-    commandHandler: {
-        enabled: true,
-        filepath: join(__dirname, './commands'),
-        /**
-         * File structure
-         * index.js
-         * commands
-         * |-> help.js, info.js, support.js
-         * node_modules
-         * package.json
-         */
-        aliases: true,
-        categories: true,
-        helpCommand: true, //Enable for help command to automatically be created and put into commands Map. NOTE: OVERRIDES HELP COMMANDS THAT ARE LOADED IN
-    }
+const client = new urmom.Client({
+  commandHandler: {
+    enabled: true,
+    filepath: join(__dirname, './commands'),
+    /**
+     * File structure
+     * index.js
+     * commands
+     * |-> help.js, info.js, support.js
+     * node_modules
+     * package.json
+     */
+    aliases: true,
+    categories: true,
+    helpCommand: true //Enable for help command to automatically be created and put into commands Map. NOTE: OVERRIDES HELP COMMANDS THAT ARE LOADED IN
+  }
 });
-urmom.on('messageCreate', (message) => {
-    message.respond();
-})
 
-urmom.start(config.token);
+client.on('messageCreate', message => message.respond());
+
+client.connect(token);
+
