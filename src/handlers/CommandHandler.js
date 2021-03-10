bafde4995a2b4fa651819e37e52a0647f2ae7fa5
JARVIS <JARVINA@student.okaloosaschools.com>
2021-03-10 13:38:40 -0600

    More command handler updates

--- src/handlers/CommandHandler.js
+++ src/handlers/CommandHandler.js
@@ -1,12 +1,14 @@
 const EventEmitter = require(`events`)
 const { readDirSync } = require(`fs`);
 const { readdirSync } = require("node:fs");
+const { defaultCommandOptions } = require(`../utils/Constants`)
 class CommandHandler extends EventEmitter {
     constructor(client) {
         this.client = client;
 
         this.commands = new Map();
         this.aliases = new Map();
+        this.categories = new Map();
         this.built = false;
 
         this.build();
@@ -15,15 +17,32 @@ class CommandHandler extends EventEmitter {
      * @returns {Map<commands>&Map<aliases>} Commands and Aliases
      */
     build(options = {}) {
-        if(this.built) return new Error(`Cannot build command handler if already initiated.`);
-        if(options.filelocation) return new TypeError(`filelocation must be defined`);
-        const commands = readdirSync()
+        if (this.built) return new Error(`Cannot build command handler if already initiated.`);
+        if (options.filelocation) return new TypeError(`filelocation must be defined`);
+        const commands = readdirSync();
     }
-    /**
-     * Rebuild command handler (reruns build)
-     */
     rebuild() {
         this.built = false;
         this.build();
+    }
+    /**
+     * Find a command, alias, category, etc. from the command handlers loaded commands.
+     * @param {string?} search 
+     * @param {array?} options
+     */
+    find(search, options) {
+        let data;
+        if (!options) options = defaultCommandOptions;
+        if (options.findtype === 'alias') data = this.aliases.get(search);
+        if (options.findtype === 'command') data = this.commands.get(search);
+        if (options.findtype === 'category') data = this.categories.get(search);
+        if (options.findtype === 'multiple') data = {
+            aliases: this.aliases.get(search),
+            commands: this.commands.get(search),
+            categories: this.categories.get(search)
+        }
+        if(options.hasPerms) data = {
+            //Filter through map of aliases + commands
+        }
     }
 }
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,7 +4,19 @@ module.exports = {
     },
     validAvatarFormats: [`jpg`, `jpeg`, `png`, `webp`, `gif`],
     baseURL: 'https://discord.com/api/v7',
+    cacheOptions: {
+        guildCache: true,
+        userCache: true,
+        guildMemberCache: true,
+        channelCache: true,
+        messageCache: false,
+    },
     defaultTimers: {
         restart: 5000, //In MS, restart after 5 seconds of execution.
+    },
+    defaultCommandOptions: {
+        findtype: '',
+        multiple: false,
+        hasPerms: null,
     }
 }
