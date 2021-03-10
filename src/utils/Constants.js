e3ed650c1fd8d8e0c98d0569eacb8f1093661a04
JARVIS <JARVINA@student.okaloosaschools.com>
2021-03-10 13:19:04 -0600

    Added default timer options

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,5 +3,8 @@ module.exports = {
         MESSAGE: 'messageCreate'
     },
     validAvatarFormats: [`jpg`, `jpeg`, `png`, `webp`, `gif`],
-    baseURL: 'https://discord.com/api/v7'
+    baseURL: 'https://discord.com/api/v7',
+    defaultTimers: {
+        restart: 5000, //In MS, restart after 5 seconds of execution.
+    }
 }
