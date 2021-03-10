09127b927e2ee26a8685b3cc607294f2bbc97be1
JARVIS <JARVINA@student.okaloosaschools.com>
2021-03-10 13:20:08 -0600

    Added restart option + timer

--- src/Client.js
+++ src/Client.js
@@ -5,8 +5,7 @@ const ws = require('ws');
 const { platform } = require('os');
 const pako = require('pako');
 const handle = require('./handlers/Event');
-const { baseURL } = require('./utils/Constants');
-
+const { baseURL, defaultTimers } = require('./utils/Constants');
 /**
  * @typedef {Object} ClientOptions
  * @property {string} token The bot's token.
@@ -124,6 +123,23 @@ class Client extends EventEmitter {
     if (!(this.socket instanceof ws)) throw new TypeError('There is no connection established to the gateway');
     this.socket.close();
     this.emit('debug', `Connection closed by '<Client>.destroy()'${reason !== undefined && reason !== null ? `, reason: ${reason}` : ''}`);
+  }
+  /**
+   * Closes connection to gateway then restarts after 5 seconds passed.
+   * @param {number?} timeout Amount of time to wait before reopening connection in milliseconds. (Minimum 5 seconds (5000ms));
+   * @param {string?} reason Reason for restart of gateway connection.
+   * @returns {client}
+   */
+  restart(timeout, reason) {
+    if(!(this.socket instanceof ws)) throw new TypeError(`No connection established to gateway to restart.`);
+    if(!timeout) timeout = defaultTimers.restart;
+    if(timeout > 5000) throw new TypeError(`Timeout cannot be below 5000ms to reconnect.`);
+    this.emit('debug', `Connection restarted by '<Client>.restart()'${reason !== undefined && reason !== null ? `, reason: ${reason}` : ''}`);
+    this.socket.close();
+    setTimeout(() => {
+      this.connect(this.token)
+    }, timeout);
+
   }
 }
 
