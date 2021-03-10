7652ea3825f545e55ccaa0a6c1262ef8a0de92ea
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-03-11 02:44:46 +0330

    Cleanups and version change
    Cleaned up some code and use Discord API version 8.

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,28 +1,28 @@
 module.exports = {
-    Events: {
-        MESSAGE: 'messageCreate'
+  Events: {
+    MESSAGE: 'messageCreate'
+  },
+  validAvatarFormats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
+  baseURL: 'https://discord.com/api/v8',
+  cacheOptions: {
+    guildCache: true,
+    userCache: true,
+    guildMemberCache: true,
+    channelCache: true,
+    messageCache: false
+  },
+  defaultTimers: {
+    restart: 5000 //In MS, restart after 5 seconds of execution.
+  },
+  defaultCommandOptions: {
+    findOptions: {
+      findtype: '',
+      multiple: false,
+      hasPerms: null
     },
-    validAvatarFormats: [`jpg`, `jpeg`, `png`, `webp`, `gif`],
-    baseURL: 'https://discord.com/api/v7',
-    cacheOptions: {
-        guildCache: true,
-        userCache: true,
-        guildMemberCache: true,
-        channelCache: true,
-        messageCache: false,
-    },
-    defaultTimers: {
-        restart: 5000, //In MS, restart after 5 seconds of execution.
-    },
-    defaultCommandOptions: {
-        findOptions: {
-            findtype: '',
-        multiple: false,
-        hasPerms: null,
-        },
-        clearOptions: {
-            clearAll: true,
-            clearByPermissions: false,
-        }
+    clearOptions: {
+      clearAll: true,
+      clearByPermissions: false
     }
-}
+  }
+};
