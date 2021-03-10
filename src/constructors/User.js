f319116558c72598f02c74429ad9de21a38b95f2
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-03-11 03:05:32 +0330

    Cleanups and fixes
    Cleaned up some redundant code and fixed coercing.

--- src/constructors/User.js
+++ src/constructors/User.js
@@ -1,34 +1,46 @@
-const Base = require("./Base");
+const Base = require('./Base');
 const { validAvatarFormats } = require('../utils/Constants');
 
 class User extends Base {
-    constructor(client, data) {
-        super();
-        this.client = client;
-        this.id = Number(data.id);
-        this.username = data.username;
-        this.tag = data.username + '#' + data.discriminator;
-        this.discriminator = data.discriminator;
-        this.bot = data.bot || false;
-        this.avatar = data.avatar;
-    }
-    
-    /**
-     * @param {object?} options Optional options for changing the avatar URL.
-     * @returns {string} the avatar URL for the user.
-     */
-    avatarURL(options) {
-        if (!this.avatar) return `https://cdn.discordapp.com/embed/avatars/${Number(this.discriminator) % 5}.png`;
-        else if (!options) return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}${this.avatar.startsWith('a_') ? '.gif' : '.png'}`;
+  constructor(client, data) {
+    super();
+    this.client = client;
+    this.id = data.id;
+    this.username = data.username;
+    this.discriminator = data.discriminator;
+    this.bot = data.bot ?? false;
+    this.avatar = data.avatar;
+    Object.assign({
+      get tag() {
+        return `${this.username}#${this.discriminator}`;
+      }
+    }, this);
+  }
+
+  /**
+   * @typedef {object} ImageURLOptions
+   * @property {boolean?} [ImageURLOptions.dynamic]
+   * @property {number?} [ImageURLOptions.size]
+   * @property {string?} [ImageURLOptions.format]
+   */
+
+  /**
+   * @param {ImageURLOptions?} [options] Optional options for changing the avatar URL.
+   * @returns {string} the avatar URL for the user.
+   */
+  avatarURL(options) {
+    if (!this.avatar) return `https://cdn.discordapp.com/embed/avatars/${this.discriminator % 5}.png`;
+    else if (!options) return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}${this.avatar.startsWith('a_') ? '.gif' : '.png'}`;
         
-        let format = (options.format || (this.avatar.startsWith('a_') ? 'gif' : 'png')).toLowerCase();
-        if (format.startsWith('.')) format = format.slice(1);
+    let format = (options.format || (this.avatar.startsWith('a_') ? 'gif' : 'png')).toLowerCase();
+    if (format.startsWith('.')) format = format.slice(1);
         
-        const size = Number(options.size) || null;
-        if (!validAvatarFormats.includes(format)) throw new TypeError(`Invalid image format: ${format}`);
-        if ((size & (size - 1)) || ((size < 16) || (size > 4096))) throw new TypeError(`The size must be a power of two between 16 and 4096.`);
+    const size = Number(options.size) || null;
+    if (!validAvatarFormats.includes(format)) throw new TypeError(`Invalid image format: ${format}`);
+    if (size & (size - 1) || (size < 16 || size > 4096)) throw new TypeError('The size must be a power of two between 16 and 4096.');
         
-        return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.${format}${size ? '?size=' + size : ''}`;
-    }
+    return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.${format}${size ? `?size=${size}` : ''}`;
+  }
 }
+
 module.exports = User;
