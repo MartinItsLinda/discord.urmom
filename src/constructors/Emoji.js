a9ad395bcbc7212d1f8a863d487f4cc0801e5228
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-03-11 08:51:02 +0700

    create Emoji and Role class, added more stuff to Message and Guild class

---
+++ src/constructors/Emoji.js
@@ -0,0 +1,17 @@
+const Base = require("./Base");
+
+class Emoji extends Base {
+	constructor(client, data) {
+		super();
+		
+		this.client = client;
+		this.id = Number(data.id);
+		this.requireColons = data.require_colons;
+		this.managed = data.managed;
+		this.animated = data.animated;
+		this.available = data.available;
+		this.roles = data.roles.map(Number);
+	}
+}
+
+module.exports = Emoji;
--- src/constructors/Message.js
+++ src/constructors/Message.js
@@ -1,26 +1,27 @@
 const Base = require("./Base");
+const User = require("./User");
 const Guild = require("./Guild");
 
 class Message extends Base {
-    constructor(client, data, channel) {
-        super();
-        /**
-         * If the message is ACTIVE (not deleted) or not.
-         * @type {boolean}
-         */
-        this.client = client;
-
-        this.active = true;
-
-        this.id = data.id;
-
-        this.createdAt = data.timestamp;
-
-        this.pinned = data.pinned;
-
-        this.attachments = data.attachments;
-
-        this.guild = new Guild(client, data.guild)
-    }
+	constructor(client, data) {
+		super();
+		this.client = client;
+		
+		this.id = Number(data.id);
+		this.content = data.content;
+		this.tts = data.tts;
+		this.mentionEveryone = data.mention_everyone;
+		this.roleMentions = data.mention_roles.map(Number);
+		this.pinned = data.pinned;
+		this.type = data.type;
+		this.author = new User(client, data.author);
+		
+		this.channel = { id: Number(data.channel_id) };
+		this.guild = new Guild(client, data.guild);
+		
+		this.createdTimestamp = new Date(data.timestamp);
+		this.editedTimestamp = data.edited_timestamp ? new Date(data.edited_timestamp) : null;
+	}
 }
+
 module.exports = Message;
