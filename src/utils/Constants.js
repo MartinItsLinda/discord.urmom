0e28ad73d7c34bb484833c0918ac0eebefac0657
Extreme <najarvis05@gmail.com>
2021-03-10 18:18:30 -0600

    added formats

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,8 +2,13 @@ module.exports = {
     Events: {
         MESSAGE: 'messageCreate'
     },
+
     validAvatarFormats: [`jpg`, `jpeg`, `png`, `webp`, `gif`],
+    validIconFormats: [`jpg`, `jpeg`, `png`, `webp`, `gif`],
+    validBannerFormats: [`jpg`, `jpeg`, `png`, `webp`],
+
     baseURL: 'https://discord.com/api/v7',
+
     cacheOptions: {
         guildCache: true,
         userCache: true,
@@ -11,18 +16,20 @@ module.exports = {
         channelCache: true,
         messageCache: false,
     },
+
     defaultTimers: {
         restart: 5000, //In MS, restart after 5 seconds of execution.
     },
+
     defaultCommandOptions: {
         findOptions: {
             findtype: '',
-        multiple: false,
-        hasPerms: null,
+            multiple: false,
+            hasPerms: null,
         },
         clearOptions: {
             clearAll: true,
             clearByPermissions: false,
         }
-    }
+    },
 }
