8d7f6166aaa0779d3c263d466b67dd79b9f7de62
Extreme <48730677+ExtremeDevelopments@users.noreply.github.com>
2021-03-10 18:20:13 -0600

    Update example.js

--- src/example.js
+++ src/example.js
@@ -1,27 +1,31 @@
-const urmom = require('discord-urmom');
-const { join } = require('path');
-// Basically your config file
-// const { token } = require('path-to-config-file');
-
-const client = new urmom.Client({
-  commandHandler: {
-    enabled: true,
-    filepath: join(__dirname, './commands'),
-    /**
-     * File structure
-     * index.js
-     * commands
-     * |-> help.js, info.js, support.js
-     * node_modules
-     * package.json
-     */
-    aliases: true,
-    categories: true,
-    helpCommand: true //Enable for help command to automatically be created and put into commands Map. NOTE: OVERRIDES HELP COMMANDS THAT ARE LOADED IN
-  }
+const c = require(`./Client`);
+const client = new c({
+    token: 'ODE5MzI0NDUwNDAyODYxMTM2.YEk9Ww.B_fgeqUnTQVeKTq0OcHl2d0_064',
+    intents: ['GUILD_MESSAGES'],
+    shardCount: 1,
+    partials: [''],
+    restWsBridgeTimeout: 1,
+    restRequestTimeout: 1,
+    restSweepInterval: 1,
+    retryLimit: 0
 });
+client.on('ready', () => {
+    client.socket.send(JSON.stringify({
+        op: 8,
 
-client.on('messageCreate', message => message.respond());
-
-client.connect(token);
-
+        d: {
+            guild_id:'818982749981114420',
+            query: "",
+            limit: 0,
+        }
+    }))
+})
+client.on('debug', console.log)
+client.on('error', console.log)
+client.on('message', m => {
+    console.log(m.attachments[0].url)
+})
+client.on('raw', (d, t) => {
+    console.log(d)
+    console.log(t)
+})
